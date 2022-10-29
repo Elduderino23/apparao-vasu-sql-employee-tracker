66a6808ef7d4a4dd6b098eb6bb37af9a86905fa8
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-28 17:20:47 -0700

    created stater files index.js, schema.sql, seeds.sql, gitignore

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+-- DROP DATABASE IF EXISTS employee_db;
+-- CREATE DATABASE employee_db;
+
+-- USE employee_db;
+
+-- CREATE TABLE department (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE employees (
+--   id INT NOT NULL,
+--   _id  INT,
+--   _details TEXT,
+--   FOREIGN KEY (customer_id)
+--   REFERENCES customer(id)
+--   ON DELETE SET NULL
+-- );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO employee_names (name)
+-- VALUES (""),
+--        (""),
+--        (""),
+--        (""),
+--        (""),
+--        (""),
+--        ("");
