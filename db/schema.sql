3136d1632ec1b56b6302481685c9062bd546163a
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-29 15:15:33 -0700

    filled in schema.sql and seed.sql with relevant code

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,33 @@
--- DROP DATABASE IF EXISTS employee_db;
--- CREATE DATABASE employee_db;
+DROP DATABASE IF EXISTS job_db;
+CREATE DATABASE job_db;
 
--- USE employee_db;
+USE job_db;
 
--- CREATE TABLE department (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   name VARCHAR(30) NOT NULL
--- );
+CREATE TABLE job_department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
 
--- CREATE TABLE employees (
---   id INT NOT NULL,
---   _id  INT,
---   _details TEXT,
---   FOREIGN KEY (customer_id)
---   REFERENCES customer(id)
---   ON DELETE SET NULL
--- );
+CREATE TABLE job_role (
+  id INT NOT NULL,
+  department_id  INT,
+  job_details TEXT,
+  title VARCHAR(30),
+  salary DECIMAL(8,2),
+  FOREIGN KEY (department_id)
+  REFERENCES job_department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE job_employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id  INT,
+  FOREIGN KEY (role_id)
+  REFERENCES job_role(id)
+  manager_id  INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES job_employee(id)
+  ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,35 @@
--- INSERT INTO employee_names (name)
--- VALUES (""),
---        (""),
---        (""),
---        (""),
---        (""),
---        (""),
---        ("");
+INSERT INTO job_department (job_department)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO job_role (title, salary, job_details, department_id)
+VALUES ("CFO", 700000.00, "Head Financial Guy", 1),
+       ("CTO", 700000.00, "Head Engineer Guy", 2),
+       ("CEO", 6000000.00, "Main Guy", 3),
+       ("CLO", 700000.00, "Head Legal Guy", 4),
+       ("Sales Manager", 600000.00, "Assistant To CFO", 1),
+       ("Technical Engineer", 600000.00, "Assistant To CTO", 2),
+       ("Financial Manager", 600000.00, "Assistant To CFO", 3),
+       ("Head Lawyer", 600000.00, "Assistant To CLO", 4),
+       ("Salesman", 70000.00, "Employee in Sales", 1),
+       ("Engineer", 70000.00, "Employee of Engineering", 2),
+       ("Financial Officer", 70000.00, "Employee of Finance", 3),
+       ("Lawyer", 70000.00, "Employee of Lawyer", 4);
+
+INSERT INTO job_employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dominic", "Conrad", 3, NULL),
+       ("Kyle", "Vance", 2, 1),
+       ("Mason", "Davis", 4, 1),
+       ("Jonathan", "Moreno", 1, 1),
+       ("Teda", "Cheng", 8, 1),
+       ("Kendrick", "Brown", 7, 1),
+       ("Carlos", "Martinez", 6, 1),
+       ("Angel", "Matias", 5, 1),
+       ("Michael", "Seaman", 9, 1),
+       ("Ying", "Ying", 10, 1),
+       ("Rebecca", "Overton", 11, 1),
+       ("Vasu", "Apparao", 12, 1),
+
+
