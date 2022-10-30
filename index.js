2a9ac02c7dee534d309b8e9824706a914d16df40
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-29 23:24:52 -0700

    askBCS troubleshoot declare function enigma in index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const jobDataBaseArray = []
 
 console.table("table the data") //
 
-// inquirer.prompt()
-
 const initialPrompt = [{
     name: "initial question",
     message: "Please make a selection",
@@ -15,6 +13,68 @@ const initialPrompt = [{
     choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
     
 }]
+
+function departmentSelect(){
+    inquirer.prompt(initialPrompt).then((response) =>{
+        switch (nextRole) {
+            case "job_department":
+                departmentView()
+                break;
+}})
+
+function roleSelect(){
+    inquirer.prompt(initialPrompt).then((response) =>{
+        switch (nextRole) {
+            case "job_role":
+                roleView()
+                break;
+}})
+
+function employeeSelect(){
+    inquirer.prompt(initialPrompt).then((response) =>{
+        switch (nextRole) {
+            case "job_employee":
+                employeeView()
+                break;
+}})
+
+
+}
+
+function departmentView(){
+    db.query('SELECT * FROM job_department', function (err, results){
+        console.table(results);
+    })
+}
+function roleView(){
+    db.query('SELECT * FROM job_role', function (err, results){
+        console.table(results);
+    })
+}
+function employeeView(){
+    db.query('SELECT * FROM job_employee', function (err, results){
+        console.table(results);
+    })
+}
+
+// function userView(nextRole) {
+//     switch (nextRole) {
+//         case "job_department":
+//             departmentView()
+//             break;
+//         case "job_role":
+//             roleView()
+//             break;
+//         case "job_employee":
+//             employeeView()
+//             break;
+//         default:
+//             return
+//     }
+// }
+
+
+
 inquirer.prompt(initialPrompt )
 
 const db = mysql.createConnection(
@@ -29,6 +89,6 @@ const db = mysql.createConnection(
     console.log(`Connected to the job_db database.`)
   );
 
-db.query('SELECT * FROM job_employee', function (err, results){
+db.query('SELECT je.id, je.department_id, je.title, je.salary, job_roles.name FROM job_employee AS je JOIN job_department ON department_id = id', function (err, results){
     console.table(results);
 })
