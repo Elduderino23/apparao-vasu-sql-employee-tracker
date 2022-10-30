9560b12ef315f8b2d175982043063b40f7987f04
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-29 20:22:33 -0700

    AskBCS troubleshoot with [ERR_Rquire_ESM] in index.js when node index.js

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-// const express = require('express');
-// const mysql = require('mysql2');
-// const consoleTables = require('console.table')
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const consoleTables = require('console.table')
+
+const jobDataBaseArray = []
+
+console.table("table the data") //
+
+inquirer.prompt()
+
+const initialPrompt = [{
+    name: "initial question",
+    message: "Please make a selection",
+    type: "list",
+    choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+    
+}]
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password
+      password: 'liquidsnake@1',
+      database: 'job_db'
+    },
+    console.log(`Connected to the job_db database.`)
+  );
+
+db.query('SELECT * FROM job_employee', function (err, results){
+    console.table(results);
+})
